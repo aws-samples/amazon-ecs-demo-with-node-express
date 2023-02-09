2f9a17ebfbaa9b48eb9537b6116ffad14befa1f7
burak-aws <74244445+burak-aws@users.noreply.github.com>
2023-02-09 08:19:07 +0100

    added back to IP

--- sample-nodejs-app/app.js
+++ sample-nodejs-app/app.js
@@ -32,7 +32,20 @@ app.post("/", function (req, res) {
       break;
   }
   console.log(calculator(num1, num2, operator));
-  res.send(`<h2> the result is : ${calculator(num1, num2, operator)}</h2>` );
+//   res.send(`<h2> the result is : ${calculator(num1, num2, operator)}</h2>` );
+  res.send(`
+    <h2>
+     That was easy, your result is: ${calculator(num1, num2, operator)}
+    </h2>
+    <img src="https://image-repo-buraku.s3.eu-west-1.amazonaws.com/EC2.png" alt="EC2-Icon"class="center" width="10%" style="vertical-align:middle;margin:0px 100px">
+    <p>
+    There's no need for compliments </p>
+    <p>I already know i'm the smartest app in the world hahaha ;)</p>
+    <p>
+    By the way I'm runningg on a single EC2 Instance
+    </p>
+    <p><a href="YOUR IP">BACK TO CALCULATOR...</a></p>
+  `);
 });
 
 app.listen(3000, function () {
